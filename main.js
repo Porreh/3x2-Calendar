d66f7c5eea248c7750fbd65d4d737fc2f958a126
Dmytro <Porreh@gmail.com>
2016-08-26 16:20:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -90,8 +90,9 @@ class Calendar {
   }
 
   nextMonth() {
-    let month = this.curDay.getMonth() + 1;
-    let year = this.curDay.getFullYear() + 1;
+    let yo = this.curDay;
+    let month = yo.getMonth() + 1;
+    let year = yo.getFullYear() + 1;
     this.html = ``;
     this.month = month;
     this.year = year;
