f21112bbebbb4238ad2deb6a543d0d8cf5c306f2
Dmytro <Porreh@gmail.com>
2016-08-30 19:06:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -100,6 +100,8 @@ function dbInterface() {
     let index = db.findIndex(x => x == value);
     db.splice(index, 1);
     this.style.backgroundColor = "#5c6bc0";
+  } if else (db.length === 3) {
+      create3x2();
   } else {
     db.push(value);
     this.style.backgroundColor = "#ec407a";
